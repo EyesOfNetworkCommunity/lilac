bcbc087adbfe980d1d8ff2a59085fd2c086acd66
Julien1498 <53144809+Julien1498@users.noreply.github.com>
2021-10-19 17:40:41 +0200

    Add nagios optimization

--- appliance/updates/3.2.sql
+++ appliance/updates/3.2.sql
@@ -4,3 +4,4 @@ UPDATE nagios_service SET action_url = '/grafana/d-solo/$_HOSTDASHID$/$HOSTNAME$
 UPDATE nagios_service SET action_url = '/grafana/d-solo/$_HOSTDASHID$/$HOSTNAME$-dashboard?orgId=1&panelId=5' WHERE id IN (187, 181);
 UPDATE nagios_service SET action_url = '/grafana/d-solo/$_HOSTDASHID$/$HOSTNAME$-dashboard?orgId=1&panelId=1' WHERE id IN (192, 195, 196, 200, 204);
 UPDATE nagios_service_template SET action_url = '/grafana/d-solo/$_HOSTDASHID$/$HOSTNAME$-dashboard?orgId=1&panelId=1' WHERE name IN ('GENERIC_GRAPH', 'TRAFFIC');
+UPDATE nagios_main_configuration SET check_result_path = '/var/spool/nagios/checkresults/';
