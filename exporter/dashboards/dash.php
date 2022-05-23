4b4baff38d95aa04c63392b9de263afa58be5bc5
Julien1498 <53144809+Julien1498@users.noreply.github.com>
2022-05-23 10:39:04 +0200

    Fix grafana dashboards

--- exporter/dashboards/dash.php
+++ exporter/dashboards/dash.php
@@ -17,9 +17,146 @@
 #
 #########################################
 */
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include_once("/srv/eyesofnetwork/eonweb/include/config.php");
 include_once("/srv/eyesofnetwork/eonweb/include/function.php");
+
+if(!empty($_GET["host"])) {
+	
+	// API URL
+	$url = 'https://127.0.0.1/eonapi/listNagiosObjects?username=admin&apiKey=' . $eon_api_token;
+	// Create a new cURL resource
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	// Setup request to send json via POST
+	$payload_service = '{"object": "services", "columns": ["description", "perf_data"], "filters": ["host_name = ' . $_GET["host"] . '"]}';
+
+	// Attach encoded JSON string to the POST fields
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $payload_service);
+	// Set the content type to application/json
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	// Return response instead of outputting
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	// Execute the POST request
+	$result = curl_exec($ch);
+	$arr_service = json_decode($result, true);
+	// Close cURL resource
+	curl_close($ch);
+	$vars = sql("lilac", "select NHCO.var_value from nagios_host_custom_object_var as NHCO INNER JOIN nagios_host as NH ON NHCO.host = NH.id WHERE NH.name = ? AND NHCO.var_name = ?", array($_GET["host"], $_GET["service"] . "_PANELID"));
+	$listLabel = array();
+	$listLabel["labels"] = array();
+	$listLabel["id"] = $vars[0][0];
+	$panelId = 1;
+	foreach($arr_service["result"]["default"] as $service) {
+		if(!empty($service["perf_data"])) {
+			$perfDatas = explode(" ", $service["perf_data"]);
+			$perfDataLabel = array();
+			foreach($perfDatas as $perfData) {
+				if($_GET["service"] == $service["description"]) {
+					$perfDataLabel = explode("=", $perfData)[0];
+					$perfDataLabelTrunc = $perfDataLabel;
+					$perfDataLabel = strtr($perfDataLabel, array("\\" => "\\\\\\\\"));
+					$perfDataLabelTrunc = strtr($perfDataLabelTrunc, array("\\" => "\\\\"));
+					$perfDataLabelTrunc = strtr($perfDataLabelTrunc, array("'" => ""));
+					$perfDataLabel = strtr($perfDataLabel, array("'" => ""));
+					array_push($listLabel["labels"], $perfDataLabel);
+				}
+
+				$panelId++;
+			}
+		}
+	}
+	print_r(json_encode($listLabel));
+}
+
+function convertUnit($unit) {
+	switch ($unit) {
+		//Time
+		case 'ns':
+		case 'µs':
+		case 'ms':
+		case 's':
+		case 'm':
+		case 'h':
+		case 'd':
+			return $unit;
+		//Data
+		case 'b':
+			return 'bits';
+			break;
+		case 'B':
+			return 'bytes';
+		case 'KB':
+		case 'KiB':
+		case 'kiB':
+		case 'kB':
+			return 'kbytes';
+		case 'MB':
+		case 'MiB':
+		case 'miB':
+		case 'mB':
+			return 'mbytes';
+		case 'GB':
+		case 'GiB':
+		case 'giB':
+		case 'gB':
+			return 'gbytes';
+		case 'Bps':
+		case 'BPS':
+		case 'BpS':
+			return 'Bps';
+		case '%':
+		case 'percent':
+		case 'pct':
+		case 'pct.':
+		case 'pc':
+			return 'percent';
+		default:
+			return 'short';
+	}
+}
+
+function getUnits() {
+// API URL
+	$url = 'http://localhost:8086/query?pretty=true&db=nagflux&q=SHOW%20series';
+
+	// Create a new cURL resource
+	$ch = curl_init($url);
+	// Return response instead of outputting
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	$results = curl_exec($ch);
+	$results = json_decode($results, true);
+	$listUnit = array();
+	foreach ($results["results"][0]["series"][0]["values"] as $result) {
+		$metricsArray = stringToDict($result[0]);
+		if(array_key_exists("unit", $metricsArray)) {
+			array_push($listUnit, $metricsArray["unit"]);
+		}
+	}
+	$listUnit = array_unique($listUnit);
+	$convertedUnit = array();
+	foreach ($listUnit as $value) {
+		array_push($convertedUnit, convertUnit($value));
+	}
+	// Close cURL resource
+	curl_close($ch);
+	return $convertedUnit;
+}
+
+function stringToDict($string) {
+	$dict = array();
+	foreach (explode(",", $string) as $message) {
+		if(str_contains($message, "unit")) {
+			$tab = explode("=", $message);
+			$dict[$tab[0]] = $tab[1];	
+		}	
+	}
+	return $dict;
+}
 
 function create_dashboard() {
 	global $grafana_api_token;
@@ -27,85 +164,85 @@ function create_dashboard() {
 
 	// API URL
 	$url = 'http://127.0.0.1:3000/api/folders/dashboards-uid';
-
 	// Create a new cURL resource
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-
 	// Setup request to send json via DELETE
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
 	// Set the content type to application/json
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Bearer ' . $grafana_api_token));
-
 	// Return response instead of outputting
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
 	// Execute the POST request
 	$result = curl_exec($ch);
-
 	$arr = json_decode($result, true);
-
 	// Close cURL resource
 	curl_close($ch);
 
 	// API URL
 	$url = 'http://127.0.0.1:3000/api/folders';
-
 	// Create a new cURL resource
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 	// Setup request to send json via POST
-
 	$payload = '{"uid": "dashboards-uid", "title": "dashboards"}';
 	// Attach encoded JSON string to the POST fields
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-
 	// Set the content type to application/json
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Bearer ' . $grafana_api_token));
-
 	// Return response instead of outputting
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
 	// Execute the POST request
 	$result = curl_exec($ch);
-
 	$arr = json_decode($result, true);
-
 	// Close cURL resource
 	curl_close($ch);
 
 	// API URL
 	$url = 'https://127.0.0.1/eonapi/listNagiosObjects?username=admin&apiKey=' . $eon_api_token;
-
 	// Create a new cURL resource
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 	// Setup request to send json via POST
-
-	$payload = '{"object" : "hosts","columns" : ["name", "services"]}';
+	$payload_host = '{"object" : "hosts", "columns" : ["check_command", "perf_data", "name"]}';
 	// Attach encoded JSON string to the POST fields
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $payload_host);
 	// Set the content type to application/json
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-
 	// Return response instead of outputting
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
 	// Execute the POST request
 	$result = curl_exec($ch);
-
-	$arr = json_decode($result, true);
-
+	$arr_host = json_decode($result, true);
 	// Close cURL resource
 	curl_close($ch);
+
+	// API URL
+	$url = 'https://127.0.0.1/eonapi/listNagiosObjects?username=admin&apiKey=' . $eon_api_token;
+	// Create a new cURL resource
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	// Setup request to send json via POST
+	$payload_service = '{"object": "services", "columns": ["host_name", "description", "check_command", "perf_data"]}';
+	// Attach encoded JSON string to the POST fields
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $payload_service);
+	// Set the content type to application/json
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	// Return response instead of outputting
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	// Execute the POST request
+	$result = curl_exec($ch);
+	$arr_service = json_decode($result, true);
+	// Close cURL resource
+	curl_close($ch);
+
 	sql("lilac", "DELETE FROM nagios_host_custom_object_var WHERE var_name = 'DASHID'");
 
-	foreach($arr["result"]["default"] as $host) {
-
+	foreach($arr_host["result"]["default"] as $host) {
 		// Close cURL resource
 		curl_close($ch);
 
@@ -117,48 +254,100 @@ function create_dashboard() {
 
 		// Setup request to send json via POST
 		$payload_1 = file_get_contents("dashboards/templates/start.json");
+		$panelId = 1;
 		
-		foreach($host["services"] as $service) {
-			$s = false;
-			switch ($service) {
-				case "processor":
-					$payload_2 = file_get_contents("dashboards/templates/processor.json");
-					$s = true;
-					break;
-				case "interfaces":
-					$payload_2 = file_get_contents("dashboards/templates/interfaces.json");
-					$s = true;
-					break;
-				case "memory":
-					$payload_2 = file_get_contents("dashboards/templates/memory.json");
-					$s = true;
-					break;
-				case "partitions":
-					$payload_2 = file_get_contents("dashboards/templates/partitions.json");
-					$s = true;
-					break;
-			}
-			if($s == true) {
-				$payload_1 = json_decode($payload_1, true);
-				$payload_2 = json_decode($payload_2, true);
-				foreach($payload_2 as $pan) {
-					array_push($payload_1[0]["dashboard"]["panels"], $pan);
+		$id = sql("lilac", "SELECT id FROM nagios_host WHERE name = ?", array($host["name"]));
+
+		foreach($arr_service["result"]["default"] as $service) {
+			if($host["name"] == $service["host_name"]) {
+				if(!empty($service["perf_data"])) {
+
+					print("<pre>".print_r($host["name"],true)."</pre>");
+					
+					$var = sql("lilac", "SELECT * FROM nagios_host_custom_object_var WHERE var_name = ? AND host = ? AND var_value = ?", array($service["description"] . "_PANELID", $id[0][0], $panelId));
+
+					if($var != null) {
+						sql("lilac", "UPDATE nagios_host_custom_object_var SET var_value = ? WHERE var_name = ? AND host = ? ", array($panelId, $service["description"] . "_PANELID", $id[0][0]));
+					} else {
+						sql("lilac", "INSERT INTO nagios_host_custom_object_var (host, var_name, var_value) VALUES (?, ?, ?)", array($id[0][0], $service["description"] . "_PANELID", $panelId));
+					}
+
+					$perfDatas = explode(" ", $service["perf_data"]);
+
+					$perfDataLabel = array();
+					foreach($perfDatas as $perfData) {
+						$payload_2 = file_get_contents("dashboards/templates/panel.json");
+						$perfDataLabel = explode("=", $perfData)[0];
+						$matches = array();
+						if(preg_match('/[A-Za-z]\w*/', explode("=", $perfData)[1], $matches)) {
+							$unit = convertUnit($matches[0]);
+						}
+						$perfDataLabelTrunc = $perfDataLabel;
+						$payload_1 = json_decode($payload_1, true);
+						$payload_2 = json_decode($payload_2, true);
+						foreach($payload_2 as $pan) {
+							array_push($payload_1[0]["dashboard"]["panels"], $pan);
+						}
+						$payload_1 = json_encode($payload_1);
+
+						$checkCommand = explode("!", $service["check_command"]);
+
+						$perfDataLabel = strtr($perfDataLabel, array("\\" => "\\\\\\\\"));
+						$perfDataLabelTrunc = strtr($perfDataLabelTrunc, array("\\" => "\\\\\\\\"));
+						$perfDataLabelTrunc = strtr($perfDataLabelTrunc, array("'" => ""));
+						$perfDataLabel = strtr($perfDataLabel, array("'" => ""));
+						
+						$payload_1 = strtr($payload_1, array('%unit%' => $unit));
+						$payload_1 = strtr($payload_1, array('%perfdata_label%' => $perfDataLabel));
+						$payload_1 = strtr($payload_1, array('%perfdata_label_trunc%' => $perfDataLabelTrunc));
+						$payload_1 = strtr($payload_1, array('%performanceLabel%' => $perfDataLabelTrunc));
+						$payload_1 = strtr($payload_1, array('%host%' => $host["name"]));
+						$payload_1 = strtr($payload_1, array('%service%' => $service["description"]));
+						$payload_1 = strtr($payload_1, array('%command%' => $checkCommand[0]));
+						$payload_1 = strtr($payload_1, array('"%id%"' => $panelId));
+						$panelId++;
+					}	
 				}
-				$payload_1 = json_encode($payload_1);
-				$payload_1 = strtr($payload_1, array('%service%' => $service));
 			}
 		}
+		
+		$perfDatas = explode(" ", $host["perf_data"]);
 
-		$payload_2 = file_get_contents("dashboards/templates/hostcheck.json");
-		$payload_2 = json_decode($payload_2, true);
-		$payload_1 = json_decode($payload_1, true);
+		$perfDataLabel = array();
+		foreach($perfDatas as $perfData) {
+			$payload_2 = file_get_contents("dashboards/templates/panel.json");
+			$perfDataLabel = explode("=", $perfData)[0];
+			$matches = array();
+			if(preg_match('/[A-Za-z]\w*/', explode("=", $perfData)[1], $matches)) {
+				$unit = convertUnit($matches[0]);
+			}
+			$perfDataLabelTrunc = $perfDataLabel;
+			$payload_1 = json_decode($payload_1, true);
+			$payload_2 = json_decode($payload_2, true);
+			foreach($payload_2 as $pan) {
+				array_push($payload_1[0]["dashboard"]["panels"], $pan);
+			}
+			$payload_1 = json_encode($payload_1);
 
-		foreach($payload_2 as $pan) {
-			array_push($payload_1[0]["dashboard"]["panels"], $pan);
+			$perfDataLabel = strtr($perfDataLabel, array("\\" => "\\\\\\\\"));
+			$perfDataLabelTrunc = strtr($perfDataLabelTrunc, array("\\" => "\\\\\\\\"));
+			$perfDataLabelTrunc = strtr($perfDataLabelTrunc, array("'" => ""));
+			$perfDataLabel = strtr($perfDataLabel, array("'" => ""));
+			
+			$payload_1 = strtr($payload_1, array('%unit%' => $unit));
+			$payload_1 = strtr($payload_1, array('%perfdata_label%' => $perfDataLabel));
+			$payload_1 = strtr($payload_1, array('%perfdata_label_trunc%' => $perfDataLabelTrunc));
+			$payload_1 = strtr($payload_1, array('%performanceLabel%' => $perfDataLabelTrunc));
+			$payload_1 = strtr($payload_1, array('%host%' => $host["name"]));
+			$payload_1 = strtr($payload_1, array('%service%' => "hostcheck"));
+			$payload_1 = strtr($payload_1, array('%command%' => $host["check_command"]));
+			$payload_1 = strtr($payload_1, array('"%id%"' => $panelId));
+			$panelId++;
 		}
 
 		$payload_2 = file_get_contents("dashboards/templates/end.json");
 		$payload_2 = json_decode($payload_2, true);
+		$payload_1 = json_decode($payload_1, true);
 
 		$tmp = $payload_1[0]["dashboard"] + $payload_2[0];
 		$payload_1[0]["dashboard"] = $tmp;
@@ -168,23 +357,20 @@ function create_dashboard() {
 
 		// Attach encoded JSON string to the POST fields
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $payload_1);
-		
 		// Set the content type to application/json
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Bearer ' . $grafana_api_token));
 		
 		// Return response instead of outputting
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		
+
 		// Execute the POST request
 		$result = curl_exec($ch);
 		$result = json_decode($result, true);
 		// Close cURL resource
 		curl_close($ch);
-
-		$id = sql("lilac", "SELECT id FROM nagios_host WHERE name = ?", array($host["name"]));
+		
 		sql("lilac", "INSERT INTO nagios_host_custom_object_var (host, var_name, var_value) VALUES (?, 'DASHID', ?)", array($id[0][0], $result["uid"]));
 	}
-  
 }
 
 ?>
